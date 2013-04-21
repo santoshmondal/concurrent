e207618c6def87cc7c0e58385f9037426fb85d80
santosh.ece06@gmail.com <santosh.ece06@gmail.com@3d56cb60-2c62-e155-2eb2-da6c9ea8befa>
2013-04-21 04:47:41 +0000

    sample jUnit test case.

---
+++ test/in/async/hibernate/misc/DynamicInsertUpdateTest.java
@@ -0,0 +1,47 @@
+package in.async.hibernate.misc;
+
+import static org.junit.Assert.fail;
+
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DynamicInsertUpdateTest {
+
+	private static final Logger log = Logger.getLogger(DynamicInsertUpdateTest.class);
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		log.info("Before Class");
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		log.info("After Class");
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		log.info("Each time::Before Test Method Called");
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		log.info("Each time::After Test Method Called");
+	}
+
+	@Test
+	public void testDynamicInsert() {
+		log.info("Testing Dynamic Insert");
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDynamicUpdate() {
+		log.info("Testing Dynamic Insert");
+		fail("Not yet implemented");
+	}
+}
