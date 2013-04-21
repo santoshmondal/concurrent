d682af6a44733c079cbc4298f257fd784f4106f1
santosh.ece06@gmail.com <santosh.ece06@gmail.com@3d56cb60-2c62-e155-2eb2-da6c9ea8befa>
2013-04-21 04:47:41 +0000

    sample jUnit test case.

---
+++ trunk/test/in/async/hibernate/misc/AllTests.java
@@ -0,0 +1,11 @@
+package in.async.hibernate.misc;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DynamicInsertUpdateTest.class })
+public class AllTests {
+
+}
